2f45ba53ced6fe0c7f0634d42363efa81dd807c9
wqrewffe <nexcodelababdullah@gmail.com>
2026-01-01 15:30:46 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig(({ mode }) => {
     preview: {
       host: '0.0.0.0',
       port: Number(process.env.PORT) || 4173,
-      allowedHosts: ['happynewyear.vercel.app'],
+      allowedHosts: ['happynewyear-chi-gilt.vercel.app'],
     },
     optimizeDeps: {
       include: [
